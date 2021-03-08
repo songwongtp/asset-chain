942709dbe0575df2a6429cb5434bd4d4f1653037
Songwong Tasneeyapant <tasneeyapant@wisc.edu>
2021-03-08 10:45:58 +0000

    client/cli

--- x/asset/client/cli/query.go
+++ x/asset/client/cli/query.go
@@ -1,16 +1,22 @@
 package cli
 
 import (
+	"context"
 	"fmt"
-	// "strings"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	// sdk "github.com/cosmos/cosmos-sdk/types"
-
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/songwongtp/asset-chain/x/asset/types"
+)
+
+const (
+	// FlagDenom is used to specify a certain asset denom
+	FlagDenom = "denom"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -25,6 +31,61 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(
+		GetAssetsCmd(),
+	)
+
+	return cmd
+}
+
+// GetAssetsCmd returns the query commands for retrieving asset info(s)
+func GetAssetsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+			Use: "assets",
+			Short: "Query for asset info(s)",
+			Long: strings.TrimSpace(
+				fmt.Sprintf(`Query the information of all assets or of a specific denomination.
+Examples:
+	$ %s query %s assets
+	$ %s query %s assets --denom=[denom]
+`,
+					version.AppName, types.ModuleName, version.AppName, types.ModuleName,
+				),
+			),
+			Args: cobra.ExactArgs(0),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				clientCtx, err := client.GetClientQueryContext(cmd)
+				if err != nil {
+					return err
+				}
+
+				denom, err := cmd.Flags().GetString(FlagDenom)
+				if err != nil {
+					return err
+				}
+
+				queryClient := types.NewQueryClient(clientCtx)
+
+				if denom == "" {
+					params := types.NewQueryAllAssetInfosRequest()
+					res, err := queryClient.AllAssetInfos(context.Background(), params)
+					if err != nil {
+						return err
+					}
+					return clientCtx.PrintProto(res)
+				}
+
+				params := types.NewQueryAssetInfoRequest(denom)
+				res, err := queryClient.AssetInfo(context.Background(), params)
+				if err != nil {
+					return err
+				}
+				return clientCtx.PrintProto(res)
+			},
+	}
+
+	cmd.Flags().String(FlagDenom, "", "The Specific asset denomination to query for")
+	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
 }
--- x/asset/client/cli/tx.go
+++ x/asset/client/cli/tx.go
@@ -2,11 +2,13 @@ package cli
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/songwongtp/asset-chain/x/asset/types"
 )
 
@@ -21,6 +23,82 @@ func GetTxCmd() *cobra.Command {
 	}
 
 	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(
+		GetBuyAssetCmd(),
+		GetSellAssetCmd(),
+	)
+
+	return cmd
+}
+
+// GetBuyAssetCmd returns the message command for buying asset
+func GetBuyAssetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+			Use: "buy [buyer] [denom] [amount]",
+			Short: `buy asset of the denom type for the given amount. Note, the'--from' flag is
+ignored as it is implied from [buyer].`,
+			Args: cobra.ExactArgs(3),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				cmd.Flags().Set(flags.FlagFrom, args[0])
+
+				clientCtx, err := client.GetClientTxContext(cmd)
+				if err != nil {
+					return err
+				}
+
+				denom := args[1]
+
+				amount, err := strconv.ParseUint(args[2], 10, 64)
+				if err != nil {
+					return err
+				}
+				
+				msg := types.NewMsgBuyAsset(clientCtx.GetFromAddress().String(), denom, amount)
+				if err := msg.ValidateBasic(); err != nil {
+					return err
+				}
+
+				return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// GetSellAssetCmd returns the message command for selling asset
+func GetSellAssetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+			Use: "sell [seller] [denom] [amount]",
+			Short: `sell asset of the denom type for the given amount. Note, the'--from' flag is
+ignored as it is implied from [buyer].`,
+			Args: cobra.ExactArgs(3),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				cmd.Flags().Set(flags.FlagFrom, args[0])
+
+				clientCtx, err := client.GetClientTxContext(cmd)
+				if err != nil {
+					return err
+				}
+
+				denom := args[1]
+
+				amount, err := strconv.ParseUint(args[2], 10, 64)
+				if err != nil {
+					return err
+				}
+
+				msg := types.NewMsgSellAsset(clientCtx.GetFromAddress().String(), denom, amount)
+				if err := msg.ValidateBasic(); err != nil {
+					return err
+				}
+
+				return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+			},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
 }
